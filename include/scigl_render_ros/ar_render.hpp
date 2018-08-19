ebdf4e791898cc393ccb4fafd611b1c12412fd0c
Tim Übelhör <TimUebelhoer@live.de>
2018-08-19 22:58:47 +0200

    implemented the ar_render class

---
+++ include/scigl_render_ros/ar_render.hpp
@@ -0,0 +1,72 @@
+#pragma once
+#include <opencv2/core/core.hpp>
+#include <geometry_msgs/TransformStamped.h>
+#include <gl3w/GL/gl3w.h>
+#include <scigl_render/gl_context.hpp>
+#include <scigl_render/render/offscreen_render.hpp>
+#include <scigl_render/scene/cv_camera.hpp>
+#include <scigl_render/scene/diffuse_light.hpp>
+#include <scigl_render/scene/model.hpp>
+#include <scigl_render/shader/shader.hpp>
+#include <scigl_render/render/texture_fullscreen_render.hpp>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/Image.h>
+
+namespace scigl_render_ros
+{
+/*!
+Augmented reality rendering of an object using the scigl_render library and
+ROS tf2 and ROS images.
+This class 
+*/
+class ArRender
+{
+public:
+  /*!
+  Configures the render class.
+  \param model_path the 3D model to render
+  \param camera_info contains the intrinsic parameters
+  \param min_depth the shortest range to measure by the camera
+  \param max_depth the longest range mo measure by the camera
+  */
+  ArRender(const std::string &model_path,
+           const sensor_msgs::CameraInfoConstPtr &camera_info,
+           int width = 640, int height = 480,
+           double min_depth = 0.01, double max_depth = 10);
+
+  /*!
+  Renders the configured model using the cameras intrinsics. The model is
+  rendered onto the image.
+  \param camera_pose the pose of the camera n the world frame (ROS convention: 
+  http://docs.ros.org/api/sensor_msgs/html/msg/CameraInfo.html)
+  \param object_pose the pose of the object in the world frame
+  \param image the image message.
+  \return an image with the object render into
+  */
+  sensor_msgs::ImageConstPtr render(
+      const geometry_msgs::TransformStamped &camera_pose,
+      const geometry_msgs::TransformStamped &object_pose,
+      const sensor_msgs::ImageConstPtr &image);
+
+private:
+  // draw offscreen
+  scigl_render::GLContext gl_context;
+  scigl_render::OffscreenRender offscreen_render;
+  cv::Mat image_buffer;
+  uint32_t seq = 0;
+  // rendering scene
+  scigl_render::CvCamera camera;
+  scigl_render::DiffuseLight light;
+  scigl_render::Model model;
+  // shaders
+  scigl_render::Shader shader;
+  scigl_render::TextureFullscreenRender image_render;
+
+  static scigl_render::CameraIntrinsics from_camera_info(
+      const sensor_msgs::CameraInfoConstPtr &camera_info,
+      double min_depth = 0.2, double max_depth = 10);
+
+  static auto convert_pose(const geometry_msgs::TransformStamped &tf_pose)
+      -> scigl_render::QuaternionPose;
+};
+} // namespace scigl_render_ros
---
+++ src/ar_render.cpp
@@ -0,0 +1,119 @@
+#include <cstring>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/core.hpp>
+#include <ros/console.h>
+#include <scigl_render/shader/single_texture_shader.hpp>
+#include <scigl_render_ros/ar_render.hpp>
+
+namespace scigl_render_ros
+{
+// image format in OpenGL
+const GLint INTERNAL_FORMAT = GL_RGB8;
+const GLenum FORMAT = GL_BGR;
+const GLenum TYPE = GL_UNSIGNED_BYTE;
+// matching format in OpenCV
+const std::string IMAGE_ENCODING = sensor_msgs::image_encodings::BGR8;
+const int CV_TYPE = CV_8UC3;
+
+ArRender::ArRender(const std::string &model_path,
+                   const sensor_msgs::CameraInfoConstPtr &camera_info,
+                   int width, int height,
+                   double min_depth, double max_depth)
+    : gl_context(false, false, width, height),
+      offscreen_render(width, height, 3 * 1, INTERNAL_FORMAT),
+      camera(from_camera_info(camera_info, min_depth, max_depth)),
+      model(model_path),
+      shader(scigl_render::SingleTextureShader::create_shader()),
+      image_render(width, height, INTERNAL_FORMAT)
+{
+  // create will allocate continous memory
+  image_buffer.create(width, height, CV_TYPE);
+  light.color = glm::vec3(1, 1, 1);
+}
+
+auto ArRender::render(const geometry_msgs::TransformStamped &camera_pose,
+                      const geometry_msgs::TransformStamped &object_pose,
+                      const sensor_msgs::ImageConstPtr &image)
+    -> sensor_msgs::ImageConstPtr
+{
+  try
+  {
+    // convert the image
+    auto cv_ptr = cv_bridge::toCvShare(image, IMAGE_ENCODING);
+    auto gl_image = cv_ptr->image;
+    cv::flip(cv_ptr->image, gl_image, 0);
+    // convert the poses
+    camera.pose = convert_pose(camera_pose);
+    light.position = camera.pose.position;
+    model.pose = convert_pose(object_pose);
+    // render the image
+    offscreen_render.clear_fbo();
+    offscreen_render.activate_fbo();
+    image_render.draw(gl_image.data, FORMAT, TYPE);
+    // render the object on top
+    camera.set_in_shader(shader);
+    light.set_in_shader(shader);
+    model.draw(shader);
+    offscreen_render.deactivate_fbo();
+    // start reading this frame
+    offscreen_render.start_read(FORMAT, TYPE);
+    // read last frame into the buffer
+    offscreen_render.read_data([this](const void *data) {
+      // buffer has continous memory (use of create)
+      memcpy(image_buffer.data, data,
+             image_buffer.total() * image_buffer.elemSize());
+    });
+  }
+  catch (cv_bridge::Exception &e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+  }
+  // create ros message
+  cv_bridge::CvImage cv_image(image->header, IMAGE_ENCODING, image_buffer);
+  cv_image.header.seq = seq++;
+  cv_image.header.stamp = ros::Time::now();
+  return cv_image.toImageMsg();
+}
+
+scigl_render::CameraIntrinsics ArRender::from_camera_info(
+    const sensor_msgs::CameraInfoConstPtr &camera_info,
+    double min_depth, double max_depth)
+{
+  scigl_render::CameraIntrinsics camera_intrinsics;
+  // dimension
+  camera_intrinsics.width = camera_info->width;
+  camera_intrinsics.height = camera_info->height;
+  // intrinsic matrix K
+  camera_intrinsics.f_x = camera_info->K[0];
+  camera_intrinsics.f_y = camera_info->K[4];
+  camera_intrinsics.c_x = camera_info->K[2];
+  camera_intrinsics.c_y = camera_info->K[5];
+  camera_intrinsics.s = 0;
+  // depth range for OpenGL
+  if (min_depth == 0)
+  {
+    // Otherwise the OpenGL z-Buffer will not work!
+    camera_intrinsics.near = 0.1;
+  }
+  else
+  {
+    camera_intrinsics.near = min_depth;
+  }
+  camera_intrinsics.far = max_depth;
+  return camera_intrinsics;
+}
+
+auto ArRender::convert_pose(const geometry_msgs::TransformStamped &tf_pose)
+    -> scigl_render::QuaternionPose
+{
+  scigl_render::QuaternionPose result;
+  result.position.x = tf_pose.transform.translation.x;
+  result.position.y = tf_pose.transform.translation.y;
+  result.position.z = tf_pose.transform.translation.z;
+  result.orientation.w = tf_pose.transform.rotation.w;
+  result.orientation.x = tf_pose.transform.rotation.x;
+  result.orientation.y = tf_pose.transform.rotation.y;
+  result.orientation.z = tf_pose.transform.rotation.z;
+  return result;
+}
+} // namespace scigl_render_ros
