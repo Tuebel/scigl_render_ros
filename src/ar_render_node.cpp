957e4e46445aaceade95af882ca3e96a68bc7453
Tim Übelhör <TimUebelhoer@live.de>
2018-08-22 23:55:34 +0200

    removed the extra main files

--- src/ar_render_node.cpp
+++ src/ar_render_node.cpp
@@ -64,3 +64,14 @@ void ArRenderNode::camera_callback(const sensor_msgs::ImageConstPtr &img,
   }
 }
 } // namespace scigl_render_ros
+
+int main(int argc, char **argv)
+{
+  // init ros
+  ros::init(argc, argv, "ar_render_node");
+  ros::NodeHandle nh;
+  scigl_render_ros::ArRenderNode render_node;
+  // will block/spin
+  render_node.run();
+  return EXIT_SUCCESS;
+}
--- src/depth_render_node.cpp
+++ src/depth_render_node.cpp
@@ -65,3 +65,14 @@ void DepthRenderNode::timer_callback(const ros::TimerEvent &)
   depth_publisher.publish(image, cam_info);
 }
 } // namespace scigl_render_ros
+
+int main(int argc, char **argv)
+{
+  // init ros
+  ros::init(argc, argv, "depth_render_node");
+  ros::NodeHandle nh;
+  scigl_render_ros::DepthRenderNode render_node;
+  // will block/spin
+  render_node.run();
+  return EXIT_SUCCESS;
+}
